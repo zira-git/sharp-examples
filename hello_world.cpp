4989e357c94cf01b28061e6886261eb57a25e945
zira <ziraproton@proton.me>
2026-02-23 23:26:55 -0500

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Input/Output stream
+using namespace std; // Uses the std namespace (standard? I think)
+
+int main() { // Main function
+    cout << "Hello, World!"; // C output stream std which outputs "Hello, World!"
+    return 0; // Returns 0 which is exit success
+}
