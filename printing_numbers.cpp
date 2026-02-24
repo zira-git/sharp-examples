31fad0e0830f76c90d89db8baf882b79076a3037
zira <ziraproton@proton.me>
2026-02-24 17:28:45 -0500

    Create printing_numbers.cpp

---
+++ printing_numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Input/Output stream
+
+int main() { // Main function
+    std::cout << 67; // You can print regular numbers
+    std::cout << 60 + 7; // You can also perform mathematical calculations
+    std::cout << 6 * 7; // Multiplication example
+    return 0; // Returns 0 (exit success)
+}
+
+// When doing things like addition, multiplication, etc... Don't use ""
